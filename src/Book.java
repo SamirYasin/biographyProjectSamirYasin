40e048128733e784e66edd1e293714191c6cf9c2
sammy yasin <yasinsammy@gmail.com>
2022-11-28 14:55:56 -0600

    homework

---
+++ src/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+
+    /**
+     * COMPLETE THIS CLASS FIRST, THEN CONTINUE WITH AUTHOR AND BIOGRAPHY
+     Create a custom constructor that will take 3 args and defines the Book object
+     Define Book class fields as name, genre, totalPage
+     */
+
+    //Create 3 args custom constructor here
+    public Book(){}
+    //Create 3 args custom constructor here
+    public Book(String name, String genre, int totalPage) {
+        this.name = name;
+        this.genre = genre;
+        this.totalPage = totalPage;
+    }
+    //Define instance variables here
+    public String name;
+    public String genre;
+    public int totalPage;
+
+    //Override toString() method that returns Book object information
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                ", totalPage=" + totalPage +
+
+                '}';
+    }
+}
